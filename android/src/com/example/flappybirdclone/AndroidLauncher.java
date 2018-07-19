37080346a96885713a889acc0798bfefcb883dae
AUser000 <dhanuhmd80@gmail.com>
2018-07-19 20:02:26 +0530

    first commit

---
+++ android/src/com/example/flappybirdclone/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.example.flappybirdclone;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.example.flappybirdclone.FlappyBirdClone;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBirdClone(), config);
+	}
+}
