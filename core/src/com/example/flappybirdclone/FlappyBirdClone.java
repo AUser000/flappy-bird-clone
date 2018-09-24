e8c054717a096e779383d60353ab04cc62aec634
AUser000 <dhanuhmd80@gmail.com>
2018-09-24 10:01:14 +0530

    modify code to relevant screen with and height

--- core/src/com/example/flappybirdclone/FlappyBirdClone.java
+++ core/src/com/example/flappybirdclone/FlappyBirdClone.java
@@ -10,69 +10,165 @@ import java.util.Random;
 
 public class FlappyBirdClone extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture background;
-	Texture[] birds;
-	Texture topTube;
-	Texture bottomTube;
-	int flapState = 0;
-	float birdY = 0;
-	float velocity = 0;
-	int gameState = 0;
-	float gravity = 0;
-	float gap = 400;
-	float maxTubeOffset = 0;
-	Random random;
-	float tubeOffset = 0;
-	float tubeVelocity = 4;
-	float tubeX;
+	Texture background;                // background image
+	Texture bird;                      // bird up
+	Texture bird2;                     // bird down
+	Texture tubeTop;                   // top tube
+	Texture tubeBotm;                  // bottom tube
+	float screenWidth;                 // width of the screen
+	float screenHeight;                // height of the screen
+	float birdPositionX;               // bird position in X axis
+	float birdPositionY;               // bird position in Y axis
+	boolean fly;                       // flying state
+	float tubeWidth;                   // with of the tube
+	boolean gameStarted;               // game state - started or not
+	boolean gameOver;                  // game state - over or not
+	float fallingHeight;				// height of the falling
+	float fallingGravityFactor;			// falling gravity factor
+	float tubeGap;						// gap between top and bottom // const
+	float tubeGapOffSets[];				// changing gap between
+	float tubePositionX[];
+	int numberOfTubes;
+	Random rand;
+	float tubeSpeed;
+	float tubeOffset;					// distance
+	int gameScore;
+	float birdWidth;                    // width of the bird
+	float birdHeight;					//
+	float[] topTubeStart;				// in y axis
+	float topTubeEnd;					//  ,,
+	float bottomTubeStart;				//  ,,
+	float[] bottomTubeEnd;				//  ,,
+	int tubeNumber;
+	boolean[] positionCondition;          // tube and bird are in same y axis or not
+    float birdXEnd;
+    float birdYEnd;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
-		birds = new Texture[2];
-	    birds[0] = new Texture("bird.png");
-	    birds[1] = new Texture("bird2.png");
-	    topTube = new Texture("toptube.png");
-	    bottomTube = new Texture("bottomtube.png");
-	    birdY = Gdx.graphics.getHeight()/ 2 - birds[0].getHeight()/2;
-	    gravity = 10;
-	    maxTubeOffset = Gdx.graphics.getHeight()/2 - gap /2 - 100;
-	    random = new Random();
-	    tubeX = Gdx.graphics.getWidth()/2 ;
+	    bird = new Texture("bird.png");
+	    bird2 = new Texture("bird2.png");
+	    tubeTop = new Texture("toptube.png");
+	    tubeBotm = new Texture("bottomtube.png");
+	    screenHeight = Gdx.graphics.getHeight();
+	    screenWidth = Gdx.graphics.getWidth();
+	    birdPositionX = screenWidth/4 - screenWidth/18;
+	    birdPositionY = screenHeight/2 - screenWidth/18;
+	    fly = true;
+	    gameStarted = false;
+	    gameOver = false;
+	    fallingHeight =  10;
+	    fallingGravityFactor = 1;
+	    tubeGap = screenHeight/3;
+	    tubeWidth = screenWidth/7;
+	    tubeSpeed = screenWidth/100;
+	    numberOfTubes = 6;
+	    rand = new Random();
+		tubeOffset = screenWidth/2;
+		tubePositionX = new float[numberOfTubes];
+		tubeGapOffSets = new float[numberOfTubes];
+		topTubeStart = new float[numberOfTubes];
+		bottomTubeEnd = new float[numberOfTubes];
+		positionCondition = new boolean[numberOfTubes];
+		gameScore = 0;
+		birdWidth = screenWidth/9;
+		birdHeight = screenWidth/11;
+		float min = (float) -0.5;
+		topTubeEnd = screenHeight/2;
+		bottomTubeStart = 0;
+		tubeNumber = -1;
+		birdXEnd = birdPositionX + birdWidth;
+	    for(int i = 0; i < numberOfTubes; i ++) {
+			tubePositionX[i] = screenWidth + i*tubeOffset;
+			tubeGapOffSets[i] = tubeGap*(rand.nextFloat()+ min);
+			// System.out.println(tubeGapOffSets[i]); // not necessary
+			topTubeStart[i] = screenHeight / 2 + tubeGap / 2 + tubeGapOffSets[i];
+			bottomTubeEnd[i] = screenHeight / 2 - tubeGap / 2 + tubeGapOffSets[i];
+			//positionCondition[i] = (birdPositionX <= tubePositionX[i]) && ();
+	    }
+
+	    // screenHeight / 2 + tubeGap / 2 + tubeGapOffSets[i]
+		// screenHeight / 2 - tubeGap / 2 + tubeGapOffSets[i]
 	}
 
 	@Override
 	public void render () {
         batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        if(gameState != 0) {
+		// before start
+		if(!gameStarted) {
             if(Gdx.input.justTouched()) {
-                velocity = -10;
-                tubeOffset = (random.nextFloat() - 0.5f)* (Gdx.graphics.getHeight() - gap -200);
+                gameStarted = true;
             }
-            if(birdY > 0 || velocity < 0) {
-                if(flapState != 0){
-                    velocity++;
-                }
-                birdY = birdY - velocity;
-            }
-        } else {
-            if(Gdx.input.justTouched()) {
-                gameState = 1;
-            }
-        }
+			// bird don't fly and fall down
+			batch.draw(bird, birdPositionX, birdPositionY ,birdWidth, birdHeight);
+		}
 
-        batch.draw(topTube, tubeX, Gdx.graphics.getHeight()/2 + gap/2 + tubeOffset, 75, Gdx.graphics.getHeight()/2);
-        batch.draw(bottomTube, tubeX, Gdx.graphics.getHeight()/2 - gap/2 - bottomTube.getHeight()/3 + tubeOffset , 75, Gdx.graphics.getHeight()/2 );
+		// playing
+		if(gameStarted && !gameOver) {
 
-	    if(flapState == 0) {
-	        flapState = 1;
-        } else {
-	        flapState = 0;
-        }
-        batch.draw(birds[flapState], Gdx.graphics.getWidth()/ 2 - birds[flapState].getWidth()/2, birdY, 65, 50);
+			// getting touch inputs
+			if(Gdx.input.justTouched()) {
+				fallingHeight = -11;
+			} else {
+				fallingHeight = fallingHeight + fallingGravityFactor;
+			}
+
+			// falling down
+			if(birdPositionY <= 0) {
+				birdPositionY = 0;
+			} else {
+				birdPositionY = birdPositionY - fallingHeight;
+			}
+
+			// flying bird
+			if(fly) {
+				batch.draw(bird, birdPositionX, birdPositionY ,birdWidth, birdHeight);
+				fly = false;
+			} else {
+				batch.draw(bird2, birdPositionX, birdPositionY,birdWidth, birdHeight);
+				fly= true;
+			}
+
+			// moving tubes
+			for (int i = 0; i < numberOfTubes; i++) {
+				if (tubePositionX[i] <= -tubeWidth) {
+					tubePositionX[i] = (numberOfTubes) * tubeOffset - tubeWidth;
+					gameScore = gameScore + 1;
+					//System.out.println(gameScore);
+				}
+				tubePositionX[i] = tubePositionX[i] - tubeSpeed;
+
+				// game over
+                // this condition for check whether tube is in the bird range
+                boolean condition = ( (birdPositionX < tubePositionX[i]) || (birdPositionX < tubePositionX[i] + tubeWidth ) )
+                        && ((birdXEnd > tubePositionX[i]) || ( birdXEnd > tubePositionX[i]));
+
+                if( (condition)) {
+                    // check bird is in between the tube
+					if ( (bottomTubeEnd[i] >= birdPositionY) || (birdPositionY + birdHeight >= topTubeStart[i]) ) {
+						gameOver = true;
+					}
+				}
+				batch.draw(tubeTop, tubePositionX[i], topTubeStart[i], tubeWidth, topTubeEnd);
+				batch.draw(tubeBotm, tubePositionX[i], bottomTubeStart, tubeWidth, bottomTubeEnd[i]);
+			}
+		}
+
+		// when game over
+		if(gameOver) {
+			// bird
+			batch.draw(bird, birdPositionX, birdPositionY ,birdWidth, birdHeight);
+			// tubes
+			for (int i = 0; i < numberOfTubes; i++) {
+				batch.draw(tubeTop, tubePositionX[i], topTubeStart[i], tubeWidth, screenHeight / 2);
+				batch.draw(tubeBotm, tubePositionX[i], 0, tubeWidth, bottomTubeEnd[i]);
+			}
+		}
+
 		batch.end();
 	}
 
